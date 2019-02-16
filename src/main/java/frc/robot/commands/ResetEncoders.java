bfe4e014b762572ef76af84d813280ec1e1e9f59
Raa'Shaun L. Hunter <RaaShaun.Hunter@Gmail.com>
2019-02-16 17:51:17 -0500

    Update for ResetEncoders,java

--- src/main/java/frc/robot/commands/ResetEncoders.java
+++ src/main/java/frc/robot/commands/ResetEncoders.java
@@ -21,6 +21,7 @@ public class ResetEncoders extends Command {
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
+    Robot.chassisSensors.resetEncoders();
   }
 
   // Called repeatedly when this Command is scheduled to run
