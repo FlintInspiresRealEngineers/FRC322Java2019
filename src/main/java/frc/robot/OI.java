73d33cdc8dfe9f5629f8687c0f75bcef7d01dd1d
Raa'Shaun Hunter <RaaShaun.Hunter@Gmail.com>
2019-03-13 23:37:34 -0400

    Add Commands to Move Cargo.

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -12,7 +12,7 @@ import frc.robot.utilities.F310Controller;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-    public JoystickButton brakeButton, liftButton, debugButton;
+    public JoystickButton brakeButton, liftButton, debugButton, suckButton, spitButton;
     public F310Controller driveStick, manipulatorStick;
 
     public OI() {
@@ -24,6 +24,10 @@ public class OI {
         debugButton = new JoystickButton(driveStick, 3);
         debugButton.toggleWhenPressed(new DebugOutput());
         manipulatorStick = new F310Controller(1);
+        suckButton = new JoystickButton(manipulatorStick, 3);
+        suckButton.whileActive(new SuckBall());
+        spitButton = new JoystickButton(manipulatorStick, 4);
+        spitButton.whileActive(new SpitBall());
         SmartDashboard.putData("ResetEncoders", new ResetEncoders());
         SmartDashboard.putData("ResetGyro", new ResetGyro());
     }
