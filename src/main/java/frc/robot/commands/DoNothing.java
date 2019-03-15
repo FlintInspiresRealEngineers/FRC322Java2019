78d85a94019c3a3aedd9eb456bff6ff75a1ad6d6
Raa'Shaun Hunter <RaaShaun.Hunter@Gmail.com>
2019-03-15 16:28:55 -0400

    Remove Chassis requirement from DoNothing Command

--- src/main/java/frc/robot/commands/DoNothing.java
+++ src/main/java/frc/robot/commands/DoNothing.java
@@ -9,7 +9,6 @@ import frc.robot.Robot;
 public class DoNothing extends Command {
 
     public DoNothing() {
-    	requires(Robot.chassis);
     }
 
     // Called just before this Command runs the first time
@@ -20,8 +19,6 @@ public class DoNothing extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
-    		Robot.chassis.autonDriveSystem(0.0, 0.0);
-    		Robot.chassis.brakesOn();
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -33,8 +30,6 @@ public class DoNothing extends Command {
     // Called once after isFinished returns true
     @Override
     protected void end() {
-    	Robot.chassis.autonDriveSystem(0.0, 0.0);
-    	Robot.chassis.brakesOff();
     }
 
     // Called when another command which requires one or more of the same
